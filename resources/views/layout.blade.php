ae976e82627be4298917812432a6be97ab1eda41
Ben Leonard <ben.leonard@creode.co.uk>
2023-11-20 14:20:15 +0000

    Made layout view file and content section configurable across different installations.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
-@extends('layout')
-@section('content')
+@extends(config('pages.layout_path'))
+@section(config('pages.content_section'))
     @foreach($content as $pageContentBlock)
         @include($pageContentBlock->view(), $pageContentBlock->attributes())
     @endforeach
